485b0c13b252478a48e2a58a21cdabc7a57e1375
manzeelanepali <nepalimanzeela@gmail.com>
2022-07-15 15:47:33 +0545

    making new component and reading it

--- react-component/src/ControlledInput.js
+++ react-component/src/ControlledInput.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Display from './Display'
+
+
 
 const ControlledInput = () => {
+
+   const [setFirst, setSecond] = useState("Hello")
+   const change=(event)=>{
+    setSecond(event.target.value)
+    
+   }
+
+   const click=()=>{
+    // alert("hey ")
+   }
   return (
-    <input/>
+    <>
+      <input  onClick={click} onChange={change} value={setFirst}/>
+      
+      <Display/>
+      <h1>{setFirst}</h1>
+</>
   )
 }
 
---
+++ react-component/src/Display.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Display = (props) => {
+  return (
+    <div>Display</div>
+  )
+}
+
+export default Display
